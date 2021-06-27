9c20f8fce679af22b45bbc4cdd391a9c5cd3bc76
Allan Carvalho <allandcarv@gmail.com>
2021-06-27 22:47:29 +0100

    feature: add login component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Admin from '../pages/Admin';
+import Login from '../pages/Login';
 import Home from '../pages/Home';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/admin" component={Admin} />
+    <Route path="/login" component={Login} />
   </Switch>
 );
 
