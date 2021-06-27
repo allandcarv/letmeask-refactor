51d107b3ce9589621b5234e7ea89798dc7ba72ca
Allan Carvalho <allandcarv@gmail.com>
2021-06-27 16:37:02 +0100

    fix: linters

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-function App() {
-  return (
-   <h1>Let Me Ask Refactor</h1>
-  );
-}
+import React from 'react';
+
+const App: React.FC = () => <h1>Let Me Ask Refactor</h1>;
 
 export default App;
