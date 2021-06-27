112e79adad770d96deed54ddfcca7a781f2a1db8
Allan Carvalho <allandcarv@gmail.com>
2021-06-27 16:52:14 +0100

    feature: add admin component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Admin from '../pages/Admin';
 import Home from '../pages/Home';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/admin" component={Admin} />
   </Switch>
 );
 
