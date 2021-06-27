c0397077dfb30e0173b811522683f67f77e1a926
Allan Carvalho <allandcarv@gmail.com>
2021-06-27 16:45:48 +0100

    feature: add react router dom and root path

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" component={} />
+  </Switch>
+);
+
+export default Routes;
