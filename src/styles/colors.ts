1c4b62655e97e8423d6f337ec4fa0eef9c191b7f
Allan Carvalho <allandcarv@gmail.com>
2021-06-28 12:50:45 +0100

    feature: add colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,27 @@
+export const colors = {
+  black: '#29292E',
+  shadow: '#050206',
+  purple: '#050206',
+  red: '#E73F5D',
+  gray: {
+    dark: '#737380',
+    medium: '#A8A8B3',
+    light: '#DBDCDD',
+  },
+  white: {
+    dark: '#F8F8F8',
+    light: '#FEFEFE',
+  },
+  pink: {
+    dark: '#E559F9',
+    light: '#D67EE2',
+  },
+  hover: {
+    purple: '#6F4BD8',
+    red: '#D73754',
+    gray: {
+      medium: '#7E7E86',
+      light: '#CECECE',
+    },
+  },
+};
