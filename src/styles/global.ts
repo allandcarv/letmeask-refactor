3f3228fe9a0cdbc9d93ab1191a574eb6cf188f76
Allan Carvalho <allandcarv@gmail.com>
2021-06-28 12:56:56 +0100

    feature: add background color and font to body

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
+import { colors } from './colors';
+
 export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     outline: 0;
+  }
+
+  body {
+    background-color: ${colors.white.dark};
+    font: 400 1rem 'Roboto', sans-serif;
   }
 `;
