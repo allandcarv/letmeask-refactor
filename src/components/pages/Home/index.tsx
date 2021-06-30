eb8dc41ae94ec55d6c3cb459dad10f46d951ab69
Allan Carvalho <allandcarv@gmail.com>
2021-06-30 14:50:35 +0100

    refactor: move home component to folder

---
+++ src/components/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../../organisms/Header';
+
+const Home: React.FC = () => (
+  <>
+    <Header />
+    <main>
+      <h1>Home</h1>
+    </main>
+  </>
+);
+export default Home;
